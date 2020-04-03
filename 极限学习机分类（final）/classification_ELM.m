6b21adf74e696f001e3565e29c9e9cf79e1411b1
yerongke <56924965+yerongke@users.noreply.github.com>
2020-04-03 15:58:33 +0800

    Add files via upload

---
+++ 极限学习机分类（final）/classification_ELM.m
@@ -0,0 +1,64 @@
+clear all
+clc
+
+%% 训练集/测试集产生
+load('RAW.mat');
+RAW1=RAW(:,:);
+RAW=RAW1(:,1:254);
+LLL=RAW1(:,255);
+[oo, pp]=size(RAW);
+temp = randperm(oo);%训练集和预测集按照3:1分类
+P_train = RAW(temp(1:300),:)';
+T_train = LLL(temp(1:300),:)';
+P_test = RAW(temp(301:end),:)';
+T_test = LLL(temp(301:end),:)';
+
+%% ELM创建/训练
+[IW,B,LW,TF,TYPE] = elmtrain(P_train,T_train,80 ,'sig',1);
+
+%% ELM仿真测试
+T_sim_1 = elmpredict(P_train,IW,B,LW,TF,TYPE);
+T_sim_2 = elmpredict(P_test,IW,B,LW,TF,TYPE);
+
+%% 结果对比
+result_1 = [T_train' T_sim_1'];
+result_2 = [T_test' T_sim_2'];
+% 训练集正确率
+k1 = length(find(T_train == T_sim_1));
+n1 = length(T_train);
+Accuracy_1 = k1 / n1 * 100;
+disp(['训练集正确率Accuracy = ' num2str(Accuracy_1) '%(' num2str(k1) '/' num2str(n1) ')'])
+% 测试集正确率
+k2 = length(find(T_test == T_sim_2));
+n2 = length(T_test);
+Accuracy_2 = k2 / n2 * 100;
+disp(['测试集正确率Accuracy = ' num2str(Accuracy_2) '%(' num2str(k2) '/' num2str(n2) ')'])
+
+%% 绘图
+figure(1)
+plot(1:300,T_train,'bo',1:300,T_sim_1,'r-*')
+grid on
+xlabel('训练集样本编号')
+ylabel('训练集集样本类别')
+string = {'训练集预测结果对比(ELM)';['(正确率Accuracy = ' num2str(Accuracy_1) '%)' ]};
+title(string)
+legend('真实值','ELM预测值')
+
+figure(2)
+plot(1:100,T_test,'bo',1:100,T_sim_2,'r-*')
+grid on
+xlabel('测试集样本编号')
+ylabel('测试集样本类别')
+string = {'测试集预测结果对比(ELM)';['(正确率Accuracy = ' num2str(Accuracy_2) '%)' ]};
+title(string)
+legend('真实值','ELM预测值')
+
+
+
+
+
+
+
+
+
+
