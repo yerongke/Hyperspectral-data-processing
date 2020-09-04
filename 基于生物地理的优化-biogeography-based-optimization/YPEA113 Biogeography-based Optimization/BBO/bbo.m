14e1ae9dbb9020ba4c134be5b43409215c8a5f99
yerongke <56924965+yerongke@users.noreply.github.com>
2020-09-04 17:09:23 +0800

    Add files via upload

---
+++ 基于生物地理的优化-biogeography-based-optimization/YPEA113 Biogeography-based Optimization/BBO/bbo.m
@@ -0,0 +1,144 @@
+%
+% Copyright (c) 2015, Yarpiz (www.yarpiz.com)
+% All rights reserved. Please read the "license.txt" for license terms.
+%
+% Project Code: YPEA113
+% Project Title: Biogeography-Based Optimization (BBO) in MATLAB
+% Publisher: Yarpiz (www.yarpiz.com)
+% 
+% Developer: S. Mostapha Kalami Heris (Member of Yarpiz Team)
+% 
+% Contact Info: sm.kalami@gmail.com, info@yarpiz.com
+%
+
+clc;
+clear;
+close all;
+
+%% Problem Definition
+
+CostFunction=@(x) Sphere(x);        % Cost Function
+
+nVar=5;             % Number of Decision Variables
+
+VarSize=[1 nVar];   % Decision Variables Matrix Size
+
+VarMin=-10;         % Decision Variables Lower Bound
+VarMax= 10;         % Decision Variables Upper Bound
+
+%% BBO Parameters
+
+MaxIt=1000;          % Maximum Number of Iterations
+
+nPop=50;            % Number of Habitats (Population Size)
+
+KeepRate=0.2;                   % Keep Rate
+nKeep=round(KeepRate*nPop);     % Number of Kept Habitats
+
+nNew=nPop-nKeep;                % Number of New Habitats
+
+% Migration Rates
+mu=linspace(1,0,nPop);          % Emmigration Rates
+lambda=1-mu;                    % Immigration Rates
+
+alpha=0.9;
+
+pMutation=0.1;
+
+sigma=0.02*(VarMax-VarMin);
+
+%% Initialization
+
+% Empty Habitat
+habitat.Position=[];
+habitat.Cost=[];
+
+% Create Habitats Array
+pop=repmat(habitat,nPop,1);
+
+% Initialize Habitats
+for i=1:nPop
+    pop(i).Position=unifrnd(VarMin,VarMax,VarSize);
+    pop(i).Cost=CostFunction(pop(i).Position);
+end
+
+% Sort Population
+[~, SortOrder]=sort([pop.Cost]);
+pop=pop(SortOrder);
+
+% Best Solution Ever Found
+BestSol=pop(1);
+
+% Array to Hold Best Costs
+BestCost=zeros(MaxIt,1);
+
+%% BBO Main Loop
+
+for it=1:MaxIt
+    
+    newpop=pop;
+    for i=1:nPop
+        for k=1:nVar
+            % Migration
+            if rand<=lambda(i)
+                % Emmigration Probabilities
+                EP=mu;
+                EP(i)=0;
+                EP=EP/sum(EP);
+                
+                % Select Source Habitat
+                j=RouletteWheelSelection(EP);
+                
+                % Migration
+                newpop(i).Position(k)=pop(i).Position(k) ...
+                    +alpha*(pop(j).Position(k)-pop(i).Position(k));
+                
+            end
+            
+            % Mutation
+            if rand<=pMutation
+                newpop(i).Position(k)=newpop(i).Position(k)+sigma*randn;
+            end
+        end
+        
+        % Apply Lower and Upper Bound Limits
+        newpop(i).Position = max(newpop(i).Position, VarMin);
+        newpop(i).Position = min(newpop(i).Position, VarMax);
+        
+        % Evaluation
+        newpop(i).Cost=CostFunction(newpop(i).Position);
+    end
+    
+    % Sort New Population
+    [~, SortOrder]=sort([newpop.Cost]);
+    newpop=newpop(SortOrder);
+    
+    % Select Next Iteration Population
+    pop=[pop(1:nKeep)
+         newpop(1:nNew)];
+     
+    % Sort Population
+    [~, SortOrder]=sort([pop.Cost]);
+    pop=pop(SortOrder);
+    
+    % Update Best Solution Ever Found
+    BestSol=pop(1);
+    
+    % Store Best Cost Ever Found
+    BestCost(it)=BestSol.Cost;
+    
+    % Show Iteration Information
+    disp(['Iteration ' num2str(it) ': Best Cost = ' num2str(BestCost(it))]);
+    
+end
+
+%% Results
+
+figure;
+%plot(BestCost,'LineWidth',2);
+semilogy(BestCost,'LineWidth',2);
+xlabel('Iteration');
+ylabel('Best Cost');
+grid on;
+
+
---
+++ 基于生物地理的优化-biogeography-based-optimization/YPEA113 Biogeography-based Optimization/BBO/main.m
@@ -0,0 +1,14 @@
+%
+% Copyright (c) 2015, Yarpiz (www.yarpiz.com)
+% All rights reserved. Please read the "license.txt" for license terms.
+%
+% Project Code: YPEA113
+% Project Title: Biogeography-Based Optimization (BBO) in MATLAB
+% Publisher: Yarpiz (www.yarpiz.com)
+% 
+% Developer: S. Mostapha Kalami Heris (Member of Yarpiz Team)
+% 
+% Contact Info: sm.kalami@gmail.com, info@yarpiz.com
+%
+
+bbo;
